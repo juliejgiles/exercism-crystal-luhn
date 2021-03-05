0ce88b0ad6f52c9558541665822fe6b979b0129f
JulieGiles <julie_j_giles@hotmail.com>
2021-03-05 11:07:55 +1100

    Refactored code as per feedback

--- spec/luhn_spec.cr
+++ spec/luhn_spec.cr
@@ -2,37 +2,37 @@ require "spec"
 require "../src/*"
 
 describe "Luhn" do
-  #Test 1
+  # Test 1
   it "single digit strings can not be valid", focus: true do
     Luhn.valid?("1").should eq(false)
   end
 
-  #Test 2
+  # Test 2
   it "a single zero is invalid", focus: true do
     Luhn.valid?("0").should eq(false)
   end
 
-  #Test 3 - CONFLICTS with Test number 8
+  # Test 3 - CONFLICTS with Test number 8
   pending "a simple valid SIN that remains valid if reversed" do
     Luhn.valid?("059").should eq(true)
   end
 
-  #Test 4
+  # Test 4
   it "a simple valid SIN that becomes invalid if reversed", focus: true do
     Luhn.valid?("59").should eq(true)
   end
 
-  #Test 5 - CONFLICTS with Test number 8
+  # Test 5 - CONFLICTS with Test number 8
   pending "a valid Canadian SIN" do
     Luhn.valid?("055 444 285").should eq(true)
   end
 
-  #Test 6
+  # Test 6
   it "invalid Canadian SIN", focus: true do
     Luhn.valid?("055 444 286").should eq(false)
   end
 
-  #Test 7
+  # Test 7
   it "invalid credit card", focus: true do
     Luhn.valid?("8273 1232 7352 0569").should eq(false)
   end
@@ -42,47 +42,47 @@ describe "Luhn" do
     Luhn.valid?("095 245 88").should eq(true)
   end
 
-  #Test number 9 - Manual test passes but not when spec is run
+  # Test number 9 - Manual test passes but not when spec is run
   pending "valid number with an odd number of spaces" do
     Luhn.valid?("234 567 891 234").should eq(true)
   end
 
-  #Test number 10
+  # Test number 10
   it "valid strings with a non-digit added at the end become invalid", focus: true do
     Luhn.valid?("059a").should eq(false)
   end
 
-  #Test number 11
+  # Test number 11
   it "valid strings with punctuation included become invalid", focus: true do
     Luhn.valid?("055-444-285").should eq(false)
   end
 
-  #Test number 12
+  # Test number 12
   it "valid strings with symbols included become invalid", focus: true do
     Luhn.valid?("055# 444$ 285").should eq(false)
   end
 
-  #Test number 13
+  # Test number 13
   it "single zero with space is invalid", focus: true do
     Luhn.valid?(" 0").should eq(false)
   end
 
-  #Test number 14 - CONFLICTS with test 8
+  # Test number 14 - CONFLICTS with test 8
   pending "more than a single zero is valid" do
     Luhn.valid?("0000 0").should eq(true)
   end
 
-  #Test number 15 - CONFLICTS with test 8
+  # Test number 15 - CONFLICTS with test 8
   pending "input digit 9 is correctly converted to output digit 9" do
     Luhn.valid?("091").should eq(true)
   end
 
-  #Test number 16
+  # Test number 16
   it "using ascii value for non-doubled non-digit isn't allowed", focus: true do
     Luhn.valid?("055b 444 285").should eq(false)
   end
 
-   #Test number 17
+  # Test number 17
   it "using ascii value for doubled non-digit isn't allowed", focus: true do
     Luhn.valid?(":9").should eq(false)
   end
--- src/luhn.cr
+++ src/luhn.cr
@@ -1,21 +1,19 @@
 class Luhn
-    @@doubled_arr = [] of Int32
-    def self.valid?(num) #Main Method - checks input format
-        num_gs = num.gsub(/\s/, "") #replaces empty splaces with no space
-        #to be valid, number must have more than 1 digit, have an even number of digits, odd number of spaces or no spaces at all, and be of string class, only contain digits or spaces, and pass the validity check method
-        if (num.count("a-zA-Z") == 0) && (/^([[:digit:]]|\]|[[:space:]]|\[|\,)*$/.match(num) != nil ) 
-            (num_gs.to_s.size > 1) && (num_gs.size % 2 == 0) && (num.count(" ")%2 == 1 || 0 ) && (num.class == String) && (num_check(num)) ? true : false 
-        else 
-            false 
-        end
-    end 
-    
-    def self.num_check(num) #Method to check validity of number combos
-        num_arr = num.gsub(/\s/, "").to_s.chars.reverse #removes white spaces and creates a reversed array of string characters of each digit
-        num_arr = num_arr.map {|x| x.to_i } #mapped to an array with items of the Number data type       
-        num_arr.each_with_index{|n, i| i%2 == 1 ? @@doubled_arr << n*2 : @@doubled_arr << n} #every second number is doubled and inserted into the doubled_arr
-        @@doubled_arr = @@doubled_arr.map {|x| x > 9 ? x-9 : x } 
-        @@doubled_arr = @@doubled_arr.reverse
-        @@doubled_arr.sum % 10 == 0 ? true : false
+  def self.valid?(number)          # Main Method - checks input format
+    number_gs = number.gsub(/\s/, "") # replaces empty splaces with no space
+    # to be valid, number must have more than 1 digit, have an even number of digits, odd number of spaces or no spaces at all, and be of string class, only contain digits or spaces, and pass the validity check method
+    if number.count("a-zA-Z:!@#$%^&&*()_+=-") == 0   #replaced Regex pattern with the count method
+      (number_gs.to_s.size > 1) && (number_gs.size % 2 == 0) && (number.count(" ") % 2 == 1 || 0) && (number_check(number)) 
+    else
+      false
     end
+  end
+
+  def self.number_check(number)    # Method to check validity of number combos
+    doubled_arr = [] of Int32                                                                 
+    number_arr = number.gsub(/\s/, "").to_s.chars.reverse.map { |x| x.to_i }    # removes white spaces and creates a reversed array of string characters of each digit and mapped to an array with items of the Number data type
+    number_arr.each_with_index { |n, i| i % 2 == 1 ? doubled_arr << n*2 : doubled_arr << n } # every second number is doubled and inserted into the doubled_arr
+    doubled_arr1 = doubled_arr.map { |x| x > 9 ? x - 9 : x }.reverse
+    doubled_arr1.sum % 10 == 0 
+  end
 end
